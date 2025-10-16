4b7ed9a5396033a6b23d4ebf6bfe6ec4d0d9313d
ekiuse <0ekiuse1@gmail.com>
2025-10-16 18:22:43 +0330

    add upload file funcyion

--- src/app/account/overview/PopupItem/AvatarPopup.tsx
+++ src/app/account/overview/PopupItem/AvatarPopup.tsx
@@ -1,34 +1,60 @@
+"use client";
+
 import { Symbol } from "@yakad/symbols";
 import { Button, Container, Hr, Row, Stack, Text } from "@yakad/ui";
-import { useRouter } from "next/navigation"
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AvatarPopup() {
     const router = useRouter();
+
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleUploadClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        alert(`Selected: ${file.name}`);
+    };
+
     return (
         <Container size="sm" align="center">
             <Symbol size={20} icon="account_circle" />
             <Stack align="center">
-                <Text>
-                    Drag photo here
-                </Text>
+                <Text>Drag photo here</Text>
                 <Hr variant="shortLine" />
                 <Row align="center">
-
-                    <Button variant="elevated" icon={<Symbol icon="upload_file" />}
-                        iconPosition="start">
+                    <Button
+                        variant="elevated"
+                        icon={<Symbol icon="upload_file" />}
+                        iconPosition="start"
+                        onClick={handleUploadClick}
+                    >
                         Upload
                     </Button>
 
-                    <Button variant="elevated" onClick={() => router.push("/camera")} icon={<Symbol icon="photo_camera" />}
-                        iconPosition="start">
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        style={{ display: "none" }}
+                        accept="image/*"
+                        onChange={handleFileChange}
+                    />
+
+                    <Button
+                        variant="elevated"
+                        onClick={() => router.push("/camera")}
+                        icon={<Symbol icon="photo_camera" />}
+                        iconPosition="start"
+                    >
                         Take Photo
                     </Button>
-
-
                 </Row>
             </Stack>
         </Container>
-
-    )
-
+    );
 }
