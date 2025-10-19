cb70d03447fb651a77099aa6b704f7f1061f707a
ekiuse <0ekiuse1@gmail.com>
2025-10-19 15:41:23 +0330

    update Change Email

--- src/app/account/security/PopupItems/change-email/ChangeEmailPopup.tsx
+++ src/app/account/security/PopupItems/change-email/ChangeEmailPopup.tsx
@@ -3,7 +3,6 @@
 import { Container } from "@yakad/ui";
 import { useSearchParams, useRouter } from "next/navigation";
 import CurrentEmailStep from "./steps/CurrentEmailStep";
-import VerifyOldEmailStep from "./steps/VerifyOldEmailStep";
 import NewEmailStep from "./steps/NewEmailStep";
 import VerifyNewEmailStep from "./steps/VerifyNewEmailStep";
 import SuccessStep from "./steps/SuccessStep";
@@ -24,8 +23,7 @@ export default function ChangeEmailPopup() {
 
     return (
         <Container align="center" size="sm">
-            {step === "current" && <CurrentEmailStep email={email} onNext={() => goToStep("verify-old")} />}
-            {step === "verify-old" && <VerifyOldEmailStep email={email} onNext={() => goToStep("new")} />}
+            {step === "current" && <CurrentEmailStep email={email} onNext={() => goToStep("new")} />}
             {step === "new" && <NewEmailStep onNext={(newEmail) => goToStep("verify-new", newEmail)} />}
             {step === "verify-new" && <VerifyNewEmailStep email={email} onNext={() => goToStep("success")} />}
             {step === "success" && <SuccessStep />}
