015597808d393759ead5da3892308eb937ae6289
amir <amirhosseinhemmt@gmail.com>
2026-08-18 09:31:01 +0330

    fix links

--- src/links.ts
+++ src/links.ts
@@ -2,8 +2,8 @@ const BASE_DOMAIN = import.meta.env.VITE_BASE_DOMAIN
 
 export const links = {
     quran: `https://quran.${BASE_DOMAIN}`,
-    blog: "https://natiq.org",
+    blog: "https://blog.natiq.org",
     sponsor: "https://sponsor.natiq.org",
     dev: "https://dev.natiq.org",
-    privacyPolicy: "https://natiq.org/privacy-policy/",
+    privacyPolicy: "https://blog.natiq.org/privacy-policy/",
 } as const
