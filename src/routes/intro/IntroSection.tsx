d82e8367bd34664e2bee18c4931e71e9359bd738
amir <amirhosseinhemmt@gmail.com>
2026-08-27 01:37:56 +0330

    up

--- src/routes/intro/IntroSection.tsx
+++ src/routes/intro/IntroSection.tsx
@@ -1,19 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { getCountries } from "@yakad/lib";
 import { Flag } from "@yakad/symbols";
-
+import { Link } from "react-router-dom";
 export function IntroSection() {
     return (
         <section className="flex justify-center px-6 pt-32">
             <div className="elevation-2 flex w-full max-w-6xl flex-col items-center gap-16 rounded-[28px] bg-surface-container p-12 md:flex-row">
                 {getCountries().map((country) => (
                     <div key={country.alpha2Code} className="h-20 w-20">
-                        {country.name}
-                        <Flag code={country.alpha2Code} />
+                        {/* {country.name}
+                        <Flag code={country.alpha2Code} /> */}
                     </div>
                 ))}
-                <Button>Login</Button>
-                <Button>Account</Button>
+                <Button asChild>
+                    <Link to="/auth">Login</Link>
+                </Button>
+                <Button>
+                    <Link to="/launcher">Account</Link>
+                </Button>
             </div>
         </section>
     );
